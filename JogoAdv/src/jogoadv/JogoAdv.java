543b70aabd042fc2053e949cb04a73b2f110159d
Igor-Bandeira08 <igordelimabandeira03@gmail.com>
2024-05-24 16:59:42 -0300

    Jogo da adivinhação

---
+++ JogoAdv/src/jogoadv/JogoAdv.java
@@ -0,0 +1,21 @@
+package jogoadv;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoAdv {
+
+    public static void formula() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner l = new Scanner(System.in);
+
+        Random na = new Random(100);
+        
+        System.out.println(na);
+
+    }
+
+}
