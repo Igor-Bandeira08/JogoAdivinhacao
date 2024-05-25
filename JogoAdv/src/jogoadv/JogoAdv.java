964d193bdbea0bff8f256b55729e2a513e41839b
Igor-Bandeira08 <igordelimabandeira03@gmail.com>
2024-05-25 02:11:59 -0300

    Código Atualizado

--- JogoAdv/src/jogoadv/JogoAdv.java
+++ JogoAdv/src/jogoadv/JogoAdv.java
@@ -7,28 +7,27 @@ public class JogoAdv {
 
     public static void main(String[] args) {
         Scanner l = new Scanner(System.in);
-        
-        int n = 1;
-        Random ale = new Random();
-        int a = ale.nextInt(10);
-        for (int i = 0;i<n; i++) {
-        System.out.println("Digite um número de 0 a 100: ");
-         n = l.nextInt();
 
         
+        int n = 0;
         
-        if (n == a){
-            System.out.println("Você adivinhou o número, parabèns!!!");
-            System.out.println(a);
-            break;
-        }else if (n < a){
-            System.out.println("O número " + n + " é menor que o numero aleatório");
-        }else if (n > a){
-            System.out.println("O número " + n + " é maior que o numero aleatório");
+        Random ale = new Random();
+        int a = ale.nextInt(10);
+
+        for (int i = -1; i <= n; i++) {
+            System.out.println("Digite um número de 0 a 10: ");
+            n = l.nextInt();
+
+            if (n == a) {
+                System.out.println("Você adivinhou o número, parabèns!!!");
+                System.out.println(a);
+                break;
+            } else if (n < a) {
+                System.out.println("O número " + n + " é menor que o numero aleatório");
+            } else if (n > a) {
+                System.out.println("O número " + n + " é maior que o numero aleatório");
+            }
         }
-       }
-        
-        
 
     }
 
