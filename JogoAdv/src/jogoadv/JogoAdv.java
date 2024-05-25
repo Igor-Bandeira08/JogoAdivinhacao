19786330c457b4b97fae090ce3607e072f7f5627
Igor-Bandeira08 <igordelimabandeira03@gmail.com>
2024-05-25 20:22:32 -0300

    Teste

--- JogoAdv/src/jogoadv/JogoAdv.java
+++ JogoAdv/src/jogoadv/JogoAdv.java
@@ -7,10 +7,26 @@ public class JogoAdv {
 
     public static void main(String[] args) {
         Scanner l = new Scanner(System.in);
+        
 
+        System.out.println("Você deseja jogar uma partida de adivinhação?");
         
+        do {
+            int o = o = l.nextInt(); ;
+            switch (o) {
+                case 1: {
+                    System.out.println("Continuar");
+                }
+                case 2: {
+                    System.out.println("Sair");
+                    
+                }
+                break;
+            }
+        } while (o >= 2);
+
         int n = 0;
-        
+
         Random ale = new Random();
         int a = ale.nextInt(10);
 
