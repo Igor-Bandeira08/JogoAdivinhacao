7d6bb6975ce25bba264dd2d96bc7f6c3dde1f859
Igor-Bandeira08 <igordelimabandeira03@gmail.com>
2024-05-27 14:48:31 -0300

    mudanca

--- JogoAdv/src/jogoadv/JogoAdv.java
+++ JogoAdv/src/jogoadv/JogoAdv.java
@@ -5,16 +5,58 @@ import java.util.Scanner;
 
 public class JogoAdv {
 
-    public static void formula() {
-
-    }
-
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner l = new Scanner(System.in);
 
-        Random na = new Random(100);
-        
-        System.out.println(na);
+        int O;
+
+        do {
+            System.out.println("Você deseja jogar uma partida de adivinhação?");
+            System.out.println("1 - Começar ");
+            System.out.println("2 - Sair    ");
+
+            O = l.nextInt();
+
+            switch (O) {
+                case 1: {
+                    System.out.println("Digite seu nome:");
+                   String nome = l.nextLine();
+                 
+                    
+                   int n = 0;
+
+                    Random ale = new Random();
+                    int a = ale.nextInt(10);
+
+                    for (int i = -1; i <= n; i++) {
+                        System.out.println("Digite um número de 0 a 10: ");
+
+                        n = l.nextInt();
+
+                        if (n == a) {
+                            System.out.println("Você adivinhou o número, parabèns!!!" + nome);
+                            System.out.println(a);
+                            break;
+                        } else if (n < a) {
+                            System.out.println("O número " + n + " é menor que o numero aleatório");
+                        } else if (n > a) {
+                            System.out.println("O número " + n + " é maior que o numero aleatório");
+                        }
+                    }
+                    break;
+                }
+                case 2: {
+                    System.out.println("Saindo");
+                    break;
+                }
+                default: {
+                    System.out.println("invalido ");
+
+                    break;
+                }
+            }
+
+        } while (O != 2);
 
     }
 
