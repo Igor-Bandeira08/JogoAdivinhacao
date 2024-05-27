bff9af14b91f14d8c49484f1823fb08b22228845
Igor-Bandeira08 <igordelimabandeira03@gmail.com>
2024-05-27 15:29:16 -0300

    Jogo

--- JogoAdv/src/jogoadv/JogoAdv.java
+++ JogoAdv/src/jogoadv/JogoAdv.java
@@ -5,29 +5,22 @@ import java.util.Scanner;
 
 public class JogoAdv {
 
-<<<<<<< HEAD
-=======
+
     public static void Jogo() {
 
     }
 
->>>>>>> 258d9b8c502ebe2a2e5c525e433ee3d78297b97a
+
     public static void main(String[] args) {
         Scanner l = new Scanner(System.in);
 
         int O;
-<<<<<<< HEAD
         
         System.out.println("Digite seu nome: ");
         String nome = l.nextLine();
         
         do {
             System.out.println("Olá " + nome + ", você deseja jogar uma partida de adivinhação?");
-=======
-
-        do {
-            System.out.println("Você deseja jogar uma partida de adivinhação?");
->>>>>>> 258d9b8c502ebe2a2e5c525e433ee3d78297b97a
             System.out.println("1 - Começar ");
             System.out.println("2 - Sair    ");
 
@@ -35,11 +28,7 @@ public class JogoAdv {
 
             switch (O) {
                 case 1: {
-<<<<<<< HEAD
                     
-=======
-
->>>>>>> 258d9b8c502ebe2a2e5c525e433ee3d78297b97a
                     int n = 0;
 
                     Random ale = new Random();
@@ -51,13 +40,8 @@ public class JogoAdv {
                         n = l.nextInt();
 
                         if (n == a) {
-<<<<<<< HEAD
                             System.out.println("Parabéns " + nome + ", você acertou o número!!!");
                             System.out.println("Número sorteado = " + a);
-=======
-                            System.out.println("Você adivinhou o número, parabèns!!!");
-                            System.out.println(a);
->>>>>>> 258d9b8c502ebe2a2e5c525e433ee3d78297b97a
                             break;
                         } else if (n < a) {
                             System.out.println("O número " + n + " é menor que o numero aleatório");
